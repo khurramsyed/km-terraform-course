370518e59fdd368584dfd6503c2b888a37f0ff6b
Khurram <mahmood.khurram@gmail.com>
2026-03-06 07:53:49 +0000

    using  random to  randomize the resource names.

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -4,7 +4,7 @@ resource "random_id" "random"{
 }
 
 resource "github_repository" "mtc_repo" {
-  name        = "mtc-repo"
+  name        = "mtc-repo-${random_id.random.dec}"
   description = "My MTC Code"
 
   visibility = "private"
